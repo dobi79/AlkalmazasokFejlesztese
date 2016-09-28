e77c6007cb8b0b5a3bb6f8b4d8d55b7a2fa4f70a
dobi79 <dobarp79@gmail.com>
2016-09-28 13:36:29 +0200

    full gyak3

--- 3.gyak/app.js
+++ 3.gyak/app.js
@@ -1,11 +1,30 @@
 var express = require('express');
 var app = express();
+var nunjucks = require('nunjucks');
 
-app.get('/', function (req, res) {
+nunjucks.configure('views', {
+    express: app,
+    autescape: true
+});
+
+app.use(function (req, res, next) {
+    console.log(req.method + ' ' + req.url);
+    next();
+});
+
+app.get('/hello/:name', function (req, res) {
+    const name = req.params.name;
+    const foo = req.query.foo;
+    res.render('master.njk', {name})
+})
+
+app.use(express.static('public'));
+
+app.get('/hello', function (req, res) {
     res.end("hello");
 })
 
 var port = process.env.PORT || 3000;
 app.listen(port, function () {
     console.log("Elindult a szerver");
-})
+});
